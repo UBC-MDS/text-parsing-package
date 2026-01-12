d608103d174bdb15980449e1b2f602031e2cda2e
Jacob Cann <jfc25@student.ubc.ca>
2026-01-12 14:51:00 -0800

    create test file for count_unique_words

---
+++ tests/unit/test_count_unique_words.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for the count_unique_words function in the count_words module.
+This test file aims to validate the output for this function given some 
+expected input data and unexpected edge case input data.
+"""
+
+import os
+import sys
+from collections import Counter
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
