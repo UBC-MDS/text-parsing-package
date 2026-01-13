f0c5c57c2b4909873325ae85da23ef88f9ff7dce
Wesley Beard <wes_beard@hotmail.com>
2026-01-12 20:31:11 -0800

    add test functions documentation

--- tests/unit/test_word_chart.py
+++ tests/unit/test_word_chart.py
@@ -32,28 +32,38 @@ base_case = word_chart(test_words)
 # test parameters
 
 def test_invalid_words_parameter():
+    """Test that a TypeError is raised when the 'words' paramenter 
+    is not of type 'dict'."""
     with pytest.raises(TypeError, match="'words' parameter is not of type 'dict'"):
         word_chart(123)
         word_chart("words")
 
 def test_invalid_display_limit_parameter():
+    """Test that a TypeError is raised when the 'display_limit' parameter
+    is not of type 'int'."""
     with pytest.raises(TypeError, match="'display_limit' parameter is not of type 'int'"):
         word_chart(test_words, display_limit="one")
 
 def test_invalid_display_order_parameter():
+    """Test that a TypeError is raised when the 'display_order' parameter
+    is not of type 'str'."""
     with pytest.raises(TypeError, match="'display_order' parameter is not of type 'str'"):
         word_chart(test_words, display_order=1)
 
 def test_invalid_display_order_values():
+    """Test that a ValueError is raised when the 'display_order' parameter
+    is not a valid option."""
     with pytest.raises(ValueError, match="'display_order must either be 'ascending' or 'descending'"):
         word_chart(test_words, display_order="reverse")
 
 # test chart structure
 
 def test_chart_is_bar():
+    """Test that the chart created is a bar chart."""
     assert base_case.mark == "bar", "Resulting chart is not a bar chart."
 
 def test_chart_titles():
+    """Test that the title reflects the most or least popular words."""
     chart_asc = word_chart(test_words, display_order="ascending")
     chart_desc = word_chart(test_words, display_order="descending")
     
@@ -67,4 +77,5 @@ def test_chart_titles():
     assert chart_desc_limit.title == "The 15 Most Popular Words"
 
 def test_chart_return_type():
+    """Test that the chart is an altair type."""
     assert isinstance(base_case, alt.vegalite.v5.api.Chart), "Returned output not an Altair chart."
