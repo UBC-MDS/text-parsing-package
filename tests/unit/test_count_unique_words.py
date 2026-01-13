22db0e3c88829642df481d48391c8e476f7d061f
Jacob Cann <jfc25@student.ubc.ca>
2026-01-12 21:10:20 -0800

    stash

--- tests/unit/test_count_unique_words.py
+++ tests/unit/test_count_unique_words.py
@@ -9,4 +9,12 @@ import sys
 from collections import Counter
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.text_parsing_package.count_words import count_unique_words
 
+def test_normal_input():
+    """
+    Test count_unique_words with a simple expected input.
+    """
+    
+    normal_input_example = 'I go where I go'
+    assert count_unique_words() == {'I': 2, 'go': 1, 'where': 1}
